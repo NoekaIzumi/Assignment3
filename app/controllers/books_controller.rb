fcbae49773c09243ebf4797a8948aea19c004056
NoekaIzumi <noeka-izumi@outlook.jp>
2023-08-14 04:55:13 +0000

    [Pause]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,24 @@
 class BooksController < ApplicationController
   def top
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    book = Book.new(book_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    book.save
+    # 4. 投稿詳細画面へリダイレクト
+    redirect_to book_path
+  end
+
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
   def edit
+
   end
 
   def show
+  @user = User.find(:id)
   end
 
   def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    # １.&2. データを受け取り新規登録するためのインスタンス作成
+    user = User.new(user_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    user.save
+    # 4. トップ画面へリダイレクト
+    redirect_to root_path
+  end
+
+  # ストロングパラメータ
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end
