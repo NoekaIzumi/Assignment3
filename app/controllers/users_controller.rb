9ef963f03da1c10b6c571a10d75b1e6f73f00197
NoekaIzumi <noeka-izumi@outlook.jp>
2023-08-14 14:30:57 +0000

    [Updated]bookers2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,30 +1,20 @@
 class UsersController < ApplicationController
+  
   def edit
-
+  @user = User.find(params[:id])
   end
 
   def show
-  @user = User.find(:id)
+  @user = User.find(params[:id])
   end
 
   def index
+    users = User.all
   end
 
-  def new
-    @user = User.new
-  end
-
-  def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    user = User.new(user_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    user.save
-    # 4. トップ画面へリダイレクト
-    redirect_to root_path
-  end
-
-  # ストロングパラメータ
+ 
   def user_params
-    params.require(:user).permit(:name, :email, :password)
+    params.require(:user).permit(:name, :profile_image)
   end
+  
 end
