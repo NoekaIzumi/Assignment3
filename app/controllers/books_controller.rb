7a3159b89d1ed027980b71aaa10586cec9346987
NoekaIzumi <noeka-izumi@outlook.jp>
2023-08-15 11:36:58 +0000

    [Updated]View/Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,9 @@ class BooksController < ApplicationController
   end
 
   def create
-    # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. 投稿詳細画面へリダイレクト
+    @book = Book.new(book_params)
+    @book.user.id=current_user.id
+    @book.save
     redirect_to book_path
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,30 @@
 class UsersController < ApplicationController
-  
+
   def edit
-  @user = User.find(params[:id])
+   @user = User.find(params[:id])
+   @books = @user.books
   end
 
   def show
-  @user = User.find(params[:id])
+   @user = User.find(params[:id])
   end
 
   def index
-    users = User.all
+   users = User.all
   end
 
- 
+
+  def update
+    @user = User.find(params[:id])
+    @user.update
+    redirect_to user_path(current_user.id)
+  end
+
+
+  private
+
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
 end
