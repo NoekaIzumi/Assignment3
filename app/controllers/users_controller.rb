de407cf2bd0d9e4a15682dc928a88699d989ef86
NoekaIzumi <noeka-izumi@outlook.jp>
2023-08-15 15:28:01 +0000

    [Updated]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,15 +16,15 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update
-    redirect_to user_path(current_user.id)
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :introduction, :images[])
   end
 
 end
