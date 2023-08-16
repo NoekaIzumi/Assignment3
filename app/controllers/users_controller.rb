a95ef275b408f79ff11071cce935871bf21ed067
NoekaIzumi <noeka-izumi@outlook.jp>
2023-08-16 10:26:23 +0000

    [updated]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,21 +7,29 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
    @books= @user.books
+   @book = Book.find(params[:id])
    #@book = Book.find(params[:id])
   end
 
   def index
    @users = User.all
    @user = User.find(current_user.id)
+   
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id=current_user.id
+    @book.save
+    redirect_to book_path(@book.id)
+  end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user)
   end
-
+  
 
   private
 
